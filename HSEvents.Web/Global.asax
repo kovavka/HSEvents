e6d2d3b41d1acf5122ba61b77ec7570e3e41e1a6
gikap <kovavka@gmail.com>
2018-09-04 09:11:59 +0500

    add project

---
+++ HSEvents.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HSEvents.WebApiApplication" Language="C#" %>
---
+++ HSEvents.Web/Server/Api/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace HSEvents.Server.Api
+{
+    public class DefaultController: ApiController
+    {
+
+        public string Get()
+        {
+            return "dfdfjddf";
+        }
+    }
+}
