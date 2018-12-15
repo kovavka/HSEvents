4d87d0ea708bcf1d318796d8a34c93b7c221bcd0
gikap <kovavka@gmail.com>
2018-12-15 20:03:58 +0500

    test

---
+++ User.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Events;
+
+namespace Domain
+{
+    public class User: Employee
+    {
+        public virtual string Login { get; set; }
+        public virtual string Password { get; set; }
+        public virtual bool IsAdmin { get; set; }
+        public virtual bool Checked { get; set; }
+    }
+}
