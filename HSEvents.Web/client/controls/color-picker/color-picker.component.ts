4cf6689f0f19b0636fb9547b917b01929c7e113a
Galina Kapger <kovavka@gmail.com>
2019-02-23 22:44:31 +0500

    add color-picker

---
+++ HSEvents.Web/client/controls/color-picker/color-picker.component.ts
@@ -0,0 +1,17 @@
+﻿import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+    moduleId: module.id.toString(),
+    selector: 'color-picker',
+    templateUrl: 'color-picker.component.html',
+    styleUrls: ['color-picker.component.css'],
+})
+export class ColorPickerComponent {
+
+    @Input()
+    hex: string;
+
+    @Output()
+    colorChange: EventEmitter<string> = new EventEmitter();
+
+}
--- HSEvents.Web/client/controls/controls.module.ts
+++ HSEvents.Web/client/controls/controls.module.ts
@@ -5,9 +5,9 @@ import { BsModalModule } from 'ng2-bs3-modal';
 import { WindowResizeEventHandlerDirective } from './directives/window-resize-event-handler.directive';
 import { DocumentClickEventHandlerDirective } from './directives/document-click-event-handler.directive';
 import { TitleInputComponent } from './title-input/title-input.component';
-import { ListRowComponent } from './list-row/list-row.component';
 import { EditableDatepickerComponent } from './editable-datapicker/editable-datapicker.component';
 import { TimepickerComponent } from './timepicker/timepicker.component';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
 
 @NgModule({
     imports: [
@@ -19,18 +19,18 @@ import { TimepickerComponent } from './timepicker/timepicker.component';
 		WindowResizeEventHandlerDirective,
 		DocumentClickEventHandlerDirective,
 		TitleInputComponent,
-		ListRowComponent,
 		EditableDatepickerComponent,
-		TimepickerComponent
+        TimepickerComponent,
+	    ColorPickerComponent
        
     ],
 	declarations: [
 		WindowResizeEventHandlerDirective,
 		DocumentClickEventHandlerDirective,
 		TitleInputComponent,
-		ListRowComponent,
 		EditableDatepickerComponent,
-		TimepickerComponent
+        TimepickerComponent,
+	    ColorPickerComponent
     ],
     providers: [
     ]
