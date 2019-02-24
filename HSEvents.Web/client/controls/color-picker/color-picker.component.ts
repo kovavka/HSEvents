bd13ca4a550b6edfb52325fce188a06fc2115df2
Galina Kapger <kovavka@gmail.com>
2019-02-24 08:45:35 +0500

    color converters

--- HSEvents.Web/client/controls/color-picker/color-picker.component.ts
+++ HSEvents.Web/client/controls/color-picker/color-picker.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component, Input, Output, EventEmitter, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
+﻿import { Component, Input, Output, EventEmitter, ChangeDetectorRef, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { RoundFunction } from '../../utilities/round-function';
 
 @Component({
@@ -7,7 +7,9 @@ import { RoundFunction } from '../../utilities/round-function';
     templateUrl: 'color-picker.component.html',
     styleUrls: ['color-picker.component.css'],
 })
-export class ColorPickerComponent {
+export class ColorPickerComponent implements AfterViewInit {
+
+    private innerHex: string;
 
     currentSelector: Selector = 'none';
     hue: number;
@@ -17,17 +19,31 @@ export class ColorPickerComponent {
     red: number;
     green: number;
     blue: number;
+    sbBackground: string;
     hueSelectorPosition: number;
     hueSelectorDiff: number;
     hueSelectorMax: number;
-    slSelectorPositionX: number;
-    slSelectorDiffX: number;
-    slSelectorPositionY: number;
-    slSelectorDiffY: number;
-    slSelectorMax: number;
+    sbSelectorPositionX: number;
+    sbSelectorDiffX: number;
+    sbSelectorPositionY: number;
+    sbSelectorDiffY: number;
+    sbSelectorMax: number;
 
     @Input()
-    hex: string;
+    set hex(value: string) {
+        if (this.innerHex !== value) {
+            this.innerHex = value;
+            if (this.hueSelector.nativeElement.clientWidth)
+                this.hueSelectorMax = this.hueSelector.nativeElement.clientWidth;
+            if (this.sbSelector.nativeElement.clientWidth)
+            this.sbSelectorMax = this.sbSelector.nativeElement.clientWidth - 5;
+            this.updateFromHex();
+        }
+    }
+
+    get hex(): string {
+        return this.innerHex;
+    }
 
     @Output()
     colorChange: EventEmitter<string> = new EventEmitter();
@@ -38,8 +54,15 @@ export class ColorPickerComponent {
     @ViewChild('hueSelector')
     hueSelector: ElementRef;
 
-    @ViewChild('slSelector')
-    slSelector: ElementRef;
+    @ViewChild('sbSelector')
+    sbSelector: ElementRef;
+
+    ngAfterViewInit() {
+        this.hueSelectorMax = 200;
+        this.sbSelectorMax = 195;
+        if (this.hex)
+            this.updateFromHex();
+    }
 
     refreshView() {
         this.recalculateColors();
@@ -55,14 +78,14 @@ export class ColorPickerComponent {
         this.refreshView();
     }
     
-    onSlSelectorMouseDown($event: any) {
+    onSbSelectorMouseDown($event: any) {
         $event.stopPropagation();
-        this.currentSelector = 'sl';
-        this.slSelectorDiffX = $event.clientX - $event.layerX + 5;
-        this.slSelectorDiffY = $event.clientY - $event.layerY + 5;
-        this.slSelectorMax = this.hueSelector.nativeElement.clientWidth - 5;
-        this.slSelectorPositionX = this.getPostion($event.clientX, this.slSelectorDiffX, -5, this.slSelectorMax);
-        this.slSelectorPositionY = this.getPostion($event.clientY, this.slSelectorDiffY, -5, this.slSelectorMax);
+        this.currentSelector = 'sb';
+        this.sbSelectorDiffX = $event.clientX - $event.layerX + 5;
+        this.sbSelectorDiffY = $event.clientY - $event.layerY + 5;
+        this.sbSelectorMax = this.sbSelector.nativeElement.clientWidth - 5;
+        this.sbSelectorPositionX = this.getPostion($event.clientX, this.sbSelectorDiffX, -5, this.sbSelectorMax);
+        this.sbSelectorPositionY = this.getPostion($event.clientY, this.sbSelectorDiffY, -5, this.sbSelectorMax);
         this.refreshView();
     }
 
@@ -74,9 +97,9 @@ export class ColorPickerComponent {
                 this.hueSelectorPosition = this.getPostion($event.clientX, this.hueSelectorDiff, 0, this.hueSelectorMax);
                 this.refreshView();
                 break;
-            case 'sl':
-                this.slSelectorPositionX = this.getPostion($event.clientX, this.slSelectorDiffX, -5, this.slSelectorMax);
-                this.slSelectorPositionY = this.getPostion($event.clientY, this.slSelectorDiffY, -5, this.slSelectorMax);
+            case 'sb':
+                this.sbSelectorPositionX = this.getPostion($event.clientX, this.sbSelectorDiffX, -5, this.sbSelectorMax);
+                this.sbSelectorPositionY = this.getPostion($event.clientY, this.sbSelectorDiffY, -5, this.sbSelectorMax);
                 this.refreshView();
                 break;
             case 'none':
@@ -96,70 +119,175 @@ export class ColorPickerComponent {
     recalculateColors() {
         //в html цветовая схема фактичести в HSB (HSV), надо переконвертировать его в HSL, RGB и HEX
         this.hue = this.getColorValue(this.hueSelectorPosition, this.hueSelectorMax, 359);
-        this.saturation = this.getColorValue(this.slSelectorDiffX, this.slSelectorMax, 100);
-        this.brightness = this.getColorValue(this.slSelectorDiffY, this.slSelectorMax, 100);
+        this.saturation = this.getColorValue(this.sbSelectorPositionX + 5, this.sbSelectorMax + 5, 100);
+        this.brightness = 100 - this.getColorValue(this.sbSelectorPositionY + 5, this.sbSelectorMax + 5, 100);
 
-        this.calculateRgb();
-                     
+        var rgb = this.calculateRgb(this.hue, this.saturation, this.brightness);
+        this.red = rgb.red;
+        this.green = rgb.green;
+        this.blue = rgb.blue;
+
+        this.updateLightness();
+
+        this.innerHex = this.getHex(rgb);
+        this.sbBackground = this.getHex(this.calculateRgb(this.hue, 100, 100));
     }
 
-    //дикий алгоритм с какого-то форума, каким-то образом работает
-    calculateRgb() {
-        var ii = Math.floor(this.hue / 60);
-        var fr = this.hue - ii;
+    updateFromHex() {
+        var rgb = this.getRgb(this.hex);
+        this.red = rgb.red;
+        this.green = rgb.green;
+        this.blue = rgb.blue
 
-        var c1 = RoundFunction.toWhole((this.brightness * (255 - this.saturation)) / 255);
-        var c2 = RoundFunction.toWhole((this.brightness * (255 - this.saturation * fr)) / 255);
-        var c3 = RoundFunction.toWhole((this.brightness * (255 - this.saturation * (1.0 - fr))) / 255);
+        this.updateLightness(); 
+        this.updateHsb();
+    }
 
-        switch (ii) {
+     //дикий алгоритм с википедии
+    updateHsb() {
+        var rgbPercent = this.rgbArray;
+        var red = rgbPercent[0];
+        var green = rgbPercent[1];
+        var blue = rgbPercent[2];
+
+        var min = Math.min(...rgbPercent);
+        var max = Math.max(...rgbPercent);
+
+        var hue: number;
+
+        if (min == max)
+            hue = 0;
+        else if (max == red && green >= blue)
+            hue = 60 * (green - blue) / (max - min) + 0;
+        else if (max == red && green < blue)
+            hue = 60 * (green - blue) / (max - min) + 360;
+        else if (max == green)
+            hue = 60 * (blue - red) / (max - min) + 120;
+        else if (max == blue)
+            hue = 60 * (red - green) / (max - min) + 240;
+
+        this.hue = RoundFunction.toWhole(hue);
+        this.saturation = max == 0 ? 0 : RoundFunction.toWhole((1 - min / max) * 100);
+        this.brightness = max;
+
+        this.hueSelectorPosition = this.getPositionFromColor(this.hue, 359, this.hueSelectorMax);
+        this.sbSelectorPositionX = this.getPositionFromColor(this.saturation, 100, this.sbSelectorMax + 5) - 5;
+        this.sbSelectorPositionY = this.getPositionFromColor(100 - this.brightness, 100, this.sbSelectorMax + 5);
+        this.sbBackground = this.getHex(this.calculateRgb(this.hue, 100, 100));
+    }   
+
+    onblur($event: any) {
+        this.hex = $event.target.value;
+    }
+
+    onkeydown($event){
+        this.hex = $event.target.value;
+}
+
+    updateLightness() {
+        var rgbPercent = this.rgbArray;
+        var lightnessPart = (Math.max(...rgbPercent) + Math.min(...rgbPercent)) / 2
+        this.lightness = RoundFunction.toWhole(lightnessPart);
+    }
+
+    decToHex(value: number) {
+        if (value > 15)
+            return value.toString(16)
+
+        return "0" + value.toString(16);
+    }
+
+    getRgb(hex: string): Rgb {
+        return <Rgb> {
+            red: parseInt(hex.slice(1, 3), 16),
+            green: parseInt(hex.slice(3, 5), 16),
+            blue: parseInt(hex.slice(5, 7), 16)
+        }
+    }
+
+
+    getHex(rgb: Rgb) {
+        return `#${this.decToHex(rgb.red)}${this.decToHex(rgb.green)}${this.decToHex(rgb.blue)}`;
+    }
+
+    get rgbArray(): number[] {
+        var red = this.red / 255 * 100;
+        var green = this.green / 255 * 100;
+        var blue = this.blue / 255 * 100;
+
+        return [red, green, blue];
+    }
+
+    //дикий алгоритм с википедии, каким-то образом работает
+    calculateRgb(hue: number , saturation: number , brightness: number): Rgb {
+        var h = Math.floor(hue / 60);
+        var vMin = (100 - saturation) * brightness / 100;
+        var a = (brightness - vMin) * (hue % 60) / 60;
+        var vInc = vMin + a;
+        var vDec = brightness - a;
+                
+        var red, green, blue: number; //в процентах
+
+        switch (h) {
             case 0:
-                this.red = this.brightness;
-                this.green = c3;
-                this.blue = c1;
+                red = brightness;
+                green = vInc;
+                blue = vMin;
                 break;
 
             case 1:
-                this.red = c2;
-                this.green = this.brightness;
-                this.blue = c1;
+                red = vDec;
+                green = brightness;
+                blue = vMin;
                 break;
 
             case 2:
-                this.red = c1;
-                this.green = this.brightness;
-                this.blue = c3;
+                red = vMin;
+                green = brightness;
+                blue = vInc;
                 break;
 
             case 3:
-                this.red = c1;
-                this.green = c2;
-                this.blue = this.brightness;
+                red = vMin;
+                green = vDec;
+                blue = brightness;
                 break;
 
             case 4:
-                this.red = c3;
-                this.green = c1;
-                this.blue = this.brightness;
+                red = vInc;
+                green = vMin;
+                blue = brightness;
                 break;
 
             case 5:
-                this.red = this.brightness;
-                this.green = c1;
-                this.blue = c2;
+                red = brightness;
+                green = vMin;
+                blue = vDec;
                 break;
-
         }
 
+        return <Rgb>{
+            red: RoundFunction.toWhole(red * 255 / 100),
+            green: RoundFunction.toWhole(green * 255 / 100),
+            blue: RoundFunction.toWhole(blue * 255 / 100)
+        };
     }
+
     getColorValue(position: number, rangeCount: number, max: number): number {
-        if (!this.hueSelectorMax)
+        if (!rangeCount)
             return 0;
 
         var value = position / rangeCount * (max + 1);
         return value > max
                 ? max
                 : RoundFunction.round(value, 0);
+    }
+
+    getPositionFromColor(value: number, rangeCount: number, max: number): number {       
+        var position = value / rangeCount * (max + 1);
+        return position > max
+            ? max
+            : RoundFunction.round(position, 0);
     }
 
     getPostion(clientX: number, diff: number, min: number, max: number): number {
@@ -175,4 +303,10 @@ export class ColorPickerComponent {
     }
 }
 
-export type Selector = 'sl' | 'hue' | 'none';
+export type Selector = 'sb' | 'hue' | 'none';
+
+export class Rgb {
+    red: number;
+    green: number;
+    blue: number;
+}
