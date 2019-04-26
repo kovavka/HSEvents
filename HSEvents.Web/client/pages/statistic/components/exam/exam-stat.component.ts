7a91a6f3c2da2ba1f65abcb7c2b0b605a11c2b13
Galina Kapger <kovavka@gmail.com>
2019-04-26 06:43:04 +0500

    add charts lib

--- HSEvents.Web/client/pages/statistic/components/exam/exam-stat.component.ts
+++ HSEvents.Web/client/pages/statistic/components/exam/exam-stat.component.ts
@@ -3,6 +3,8 @@ import { AuthService } from '../../../../services/auth.service';
 import { AbstractComponent } from '../../../../utilities/abstract.component';
 import { StatisticService } from '../../statistic.service';
 
+declare var Chartist: any;
+
 @Component({
     moduleId: module.id.toString(),
     templateUrl: 'exam-stat.component.html'
@@ -22,8 +24,32 @@ export class ExamStatComponent extends AbstractComponent implements OnInit {
             .takeUntil(this.ngUnsubscribe)
             .subscribe(data => {
                 this.data = data;
-                console.log(data);
-            });
+                console.log(data[0].value[0].value);
 
+
+
+                var chartData = {
+                    labels: [
+                        '0-10',
+                        '11-20',
+                        '21-30',
+                        '31-40',
+                        '41-50',
+                        '51-60',
+                        '61-70',
+                        '71-80',
+                        '81-90',
+                        '91-100',
+                    ],
+                    series: [
+                        [...data[0].value[0].value],
+                        [...data[1].value[0].value],
+                    ]
+                    
+                };
+                new Chartist.Bar('.ct-chart', chartData);
+
+
+            });
     }
 }
